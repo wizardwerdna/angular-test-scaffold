501744bf782f85a5c3df66cefae9ffa9b0269201
Andrew C. Greenberg <wizardwerdna@gmail.com>
2013-10-06 23:27:52 -0700

    support ngClickLink

--- plugin.js
+++ plugin.js
@@ -1,3 +1,6 @@
+'use strict';
+/* global $ */
+/* global inject */
 $(function(){
 
   $.fn.ngBrowserTrigger = function(eventType) {
@@ -7,7 +10,7 @@ $(function(){
     if (document.createEvent) {
       
       var event = document.createEvent('MouseEvents');
-      event.initMouseEvent(eventType, true, true, window, 
+      event.initMouseEvent(eventType, true, true, window,
         0, 0, 0, 0, 0, false, false, false, false, 0, element);
       element.dispatchEvent(event);
 
@@ -17,13 +20,22 @@ $(function(){
 
     }
 
-  }
+  };
+
+  $.fn.ngClickLink = function() {
+    var element = this;
+    var href = element.attr('href');
+    element.scope().$apply(inject(function($location){
+      element.click();
+      $location.path(href);
+    }));
+  };
 
   $.fn.ngTypeOn = function(string) {
 
     this.val(string);
     this.ngBrowserTrigger('input');
 
-  }
+  };
 
 });
